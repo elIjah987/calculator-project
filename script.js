fef6842733dcb508d73a313dc7ce2517c8b57226
COR Students <corstudents@Matts-MBP-53.localdomain>
2018-10-30 13:41:47 -0400

    Calculator Project

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    let counter = 0;
+    $("#oneButton").click(function(){
+        $("#calculatorDisplay").html(counter = 1);
+        
+             });
+    $("#clearButton").click(function(){
+        $("#calculatorDisplay").html(counter = 0);
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
